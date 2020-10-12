4f288035de2fa999f56dc4c92485bfecfd06ea8c
dingga <dingga270499@gmail.com>
2020-10-12 19:55:11 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // halo
 import 'package:flutter/material.dart';
+//dak bisa
 
 void main() {
   runApp(MyApp());
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title:'' ),
+      home: MyHomePage(title: ''),
     );
   }
 }
